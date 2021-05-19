9d9d0c08428c397180372cb3160e7e18e63a7824
Phil Wang <lucidrains@gmail.com>
2021-05-19 08:10:16 -0700

    0.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'g-mlp-pytorch',
   packages = find_packages(),
-  version = '0.0.8',
+  version = '0.0.9',
   license='MIT',
   description = 'gMLP - Pytorch',
   author = 'Phil Wang',
